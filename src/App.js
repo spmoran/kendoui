85652e2d065ddf4b9733f65576810ceab777af80
spmoran <spmoran.dev@gmail.com>
2022-05-14 13:15:03 -0700

    added placeholder component to ensure table state preserved on page change

--- src/App.js
+++ src/App.js
@@ -1,91 +1,30 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Table from "./components/Table";
+import Placeholder from "./components/Placeholder";
+
 import "./App.css";
 import "@progress/kendo-theme-default/dist/all.css";
-import testData from "./mock-data.json";
-import { process } from "@progress/kendo-data-query";
-import { Grid, GridColumn, GridToolbar } from "@progress/kendo-react-grid";
-import { ExcelExport } from "@progress/kendo-react-excel-export";
-import { useRef, useState } from "react";
 
 function App() {
-  const [dataState, setDataState] = useState({
+  const [tableState, setTableState] = useState({
     skip: 0,
-    take: 10,
+    take: 25,
     sort: [],
     group: [],
   });
 
-  const [dataResult, setDataResult] = useState(process(testData, dataState));
-  const _export = useRef(null);
-
-  const onDataStateChange = (event) => {
-    setDataResult(process(testData, event.dataState));
-
-    setDataState(event.dataState);
-
-    /*  setDataState((prevState) => {
-      const stateData = { ...prevState };
-      stateData.group = event.dataState.group;
-      stateData.skip = event.dataState.skip;
-      stateData.take = event.dataState.take;
-      stateData.filter = event.dataState.filter;
-      if (dataState.sort.length > 1){
-        stateData.sort = event.dataState.sort
-      }
-    });
-    */
-  };
-
-  const exportExcel = (props) => {
-    if (_export.current !== null) {
-      console.log(dataResult);
-      _export.current.save();
-    }
-  };
-
   return (
     <div className="App">
-      <div style={{ margin: "10px 30px" }}>
-        <h1>Hello KendoReact!</h1>
-        <ExcelExport
-          data={dataResult.data}
-          group={dataState.group}
-          ref={_export}
-        >
-          <Grid
-            style={{ maxHeight: "800px", width: "100%" }}
-            data={dataResult}
-            pageable={{
-              buttonCount: 5,
-              pageSizes: [10, 25, 50, 100, testData.length],
-            }}
-            filterable={true}
-            groupable={true}
-            reorderable={true}
-            sortable={{ mode: "multiple" }}
-            {...dataState}
-            //total={testData.length}
-            onDataStateChange={onDataStateChange}
-          >
-            <GridToolbar>
-              <button
-                title="Export to Excel"
-                className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary"
-                onClick={exportExcel}
-              >
-                Export to Excel
-              </button>
-            </GridToolbar>
-            <GridColumn field="id" title="ID" width="90" filterable={false} />
-            <GridColumn field="first_name" title="First Name" />
-            <GridColumn field="last_name" title="Last Name" />
-            <GridColumn field="email" title="Email Address" />
-            <GridColumn field="gender" title="Gender" />
-            {/*<GridColumn field="ip_address" />*/}
-            <GridColumn field="Date" title="Hire Date" />
-            <GridColumn field="Citizenship" title="Primary Citizenship" />
-          </Grid>
-        </ExcelExport>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Table tableState={tableState} setTableState={setTableState} />
+          }
+        />
+        <Route path="/dud" element={<Placeholder />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Placeholder.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Placeholder = () => {
+  return (
+    <div>
+      <Link to="/" className="btn btn-primary">
+        Go Home
+      </Link>
+      ;
+    </div>
+  );
+};
+
+export default Placeholder;
